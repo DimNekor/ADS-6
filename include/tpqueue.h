7feaaa0c7a2340bf8c10b24de60872ad43dc55fc
DimNekor <117307225+DimNekor@users.noreply.github.com>
2023-04-13 23:36:08 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,125 +1,107 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
-#include <string>
 
-#pragma once
+#include <string>
 #include <iostream>
 
 struct SYM {
   char ch;
   int prior;
-  SYM* next;
 };
 
-template <typename T, int size>
+template<typename T, int size>
 class TPQueue {
  private:
-  struct T {
-    char ch;
-    int prior;
-    T* next;
-  };
-  T *head;
-  static int members;
-  int size;
-  TPQueue::T* create(const char&, const int&);
+  T* mas;
+  int head, tail, count;
 
  public:
   TPQueue();
-  int countMembers() {
-    members++;
-    return members;
-  };
-  int countMembers1() {
-    members--;
-    return members;
-  }
-  int push(SYM);
-  int getCurSize();
-  bool isFull();
   bool isEmpty();
-  SYM pop();
-  void display();
-  
+  bool isFull();
+  int push(T);
+  T pop();
+  ~TPQueue();
 };
+
+
 template<typename T, int size>
-int TPQueue<T, size>::members = 0;
-
-template <typename T, int size>
 TPQueue<T, size>::TPQueue() {
-  head = nullptr;
-}
-template <typename T, int size>
-int TPQueue<T, size>::getCurSize() {
-  return members;
-}
-
-template <typename T, int size>
-typename TPQueue<T, size>::T* TPQueue<T, size>::create(const char& ch,
-                                                       const int& prior) {
-  T* temp = new T;
-  temp->ch = ch;
-  temp->prior = prior;
-  temp->next = nullptr;
-  return temp;
+  mas = new T[size];
+  head = 0;
+  tail = 0;
+  count = 0;
 }
 
-template <typename T, int size>
+template<typename T, int size>
 bool TPQueue<T, size>::isEmpty() {
-  if (head == nullptr) return true;
-  return false;
-}
-template <typename T, int size>
-bool TPQueue<T, size>::isFull() {
-  if (getCurSize() == size)
-    return true;
-  else
-    return false;
+  return count == 0;
 }
 
-template <typename T, int size>
-int TPQueue<T, size>::push(SYM item) {
-  if (isEmpty()) {
-    head = create(item.ch, item.prior);
-    countMembers();
-  }
-  else if (!isFull()) {
-    T* temp = create(item.ch, item.prior);
-    int counter = members;
-    T* current = head;
-    if (current->prior < temp->prior) {
-      temp->next = current;
-      head = temp;
-      countMembers();
+template<typename T, int size>
+bool TPQueue<T, size>::isFull() {
+  return count == size;
+}
+
+template<typename T, int size>
+int TPQueue<T, size>::push(T data) {
+  if (isFull())
+    throw std::string("Full!");
+  else if (isEmpty()) {
+    count++;
+    mas[head % size] = data;
+  } else {
+    int current = head;
+    int counter = count;
+    if (mas[current % size].prior < data.prior) {
+      int currT = tail;
+      while (counter) {
+        mas[(currT + 1) % size] = mas[(currT) % size];
+        counter--;
+        currT--;
+      }
+      count++;
+      tail++;
+      mas[head % size] = data;
       return 1;
     }
     while (counter - 1) {
-      if (current->prior >= temp->prior && current->next->prior < temp->prior) {
-        temp->next = current->next;
-        current->next = temp;
-        countMembers();
+      if (mas[current % size].prior >= data.prior &&
+          mas[(current + 1) % size].prior < data.prior) {
+        int flag = current + 1;
+        int currT = tail;
+        while (counter - 1) {
+          mas[(currT + 1) % size] = mas[(currT) % size];
+          counter--;
+          currT--;
+        }
+        mas[flag % size] = data;
+        count++;
+        tail++;
         return 1;
       }
-      current = current->next;
-      counter -= 1;
+      counter--;
+      current++;
     }
-    current->next = temp;
-    temp->next = head;
-    countMembers();
+    mas[(tail + 1) % size] = data;
+    tail++;
+    count++;
     return 1;
-    }
   }
-template <typename T, int size>
-SYM TPQueue<T, size>::pop() {
-  if (!isEmpty()) {
-    T temp;
-    temp.ch = head->ch;
-    temp.prior = head->prior;
-    temp.next = nullptr;
-    head = head->next;
-    return *temp;
-  }
+}
+
+template<typename T, int size>
+T TPQueue<T, size>::pop() {
+  T temp = mas[head];
+  head++;
+  count--;
+  return temp;
+}
+
+template<typename T, int size>
+TPQueue<T, size>::~TPQueue() {
+  delete[] mas;
 }
 
 #endif  // INCLUDE_TPQUEUE_H_
